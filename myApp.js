9d6b5de46ac1c6043f8fb562777ce7bb46c970f6
RogersCodes <adindajohnrogers@gmail.com>
2024-12-14 18:35:03 +0300

    feat: modified my app.js

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
 
 
 let Person;
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
